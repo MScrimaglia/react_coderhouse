9c757d7cbc7e992690b4cccc61ee5c11a272aa19
MScrimaglia <maxiscrimaglia@gmail.com>
2023-01-18 17:27:02 -0300

    item list fix

--- src/components/cart-item/index.jsx
+++ src/components/cart-item/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import './style.css'
 
 const CartItem = (props) => {
@@ -8,20 +8,49 @@ const CartItem = (props) => {
     let purchases = JSON.parse(localStorage.getItem('ch_products_cart'));
     let purchase;
 
+    for (let el of purchases){
+        if (el.id == product.id){
+            purchase = el;
+        }
+    }
+
+    const [item_qty, setItemQty] = useState(purchase.qty);
+
+    window.addEventListener('storage', () => {
+        purchases = JSON.parse(localStorage.getItem('ch_products_cart'));
+
         for (let el of purchases){
             if (el.id == product.id){
                 purchase = el;
             }
         }
+        setItemQty(purchase.qty);
+        console.log(purchase.qty);
+    })
 
-    useEffect(() => {
-        document.getElementById('remove-' + purchase.id).addEventListener('click', () => {
-            delete purchases[purchases.indexOf(purchase)];
-            purchases = purchases.filter(item => item != undefined);
-            localStorage.setItem('ch_products_cart', JSON.stringify(purchases));
-            window.dispatchEvent( new Event('storage') );
-        })
-    });
+    const removeHandler = () => {
+        delete purchases[purchases.indexOf(purchase)];
+        purchases = purchases.filter(item => item != undefined);
+        localStorage.setItem('ch_products_cart', JSON.stringify(purchases));
+        window.dispatchEvent( new Event('storage') );
+    }
+
+    const subtractHandler = () => {
+        if (purchase.qty <= 1) {return;}
+        purchase.qty -= 1;
+        purchases[purchases.indexOf(purchase)] = purchase;
+        localStorage.setItem('ch_products_cart', JSON.stringify(purchases));
+        setItemQty(purchase.qty);
+        window.dispatchEvent( new Event('storage') );
+    }
+
+    const addHandler = () => {
+        purchase.qty += 1;
+        purchases[purchases.indexOf(purchase)] = purchase;
+        localStorage.setItem('ch_products_cart', JSON.stringify(purchases));
+        setItemQty(purchase.qty);
+        window.dispatchEvent( new Event('storage') );
+    }
 
     
 
@@ -33,13 +62,13 @@ const CartItem = (props) => {
                 <div className="item-price">
                     <h4 className="item-price-text">${parseInt(purchase.price) * parseInt(purchase.qty)}</h4>
                     <div className="item-change-qty horizontal-container">
-                        <button type="button" className="item-qty-btn">-</button>
-                        <p id="qty">{purchase.qty}</p>
-                        <button type="button" className="item-qty-btn">+</button>
+                        <button id={'subtract-' + purchase.id} onClick={subtractHandler} type="button" className="item-qty-btn">-</button>
+                        <p id="qty">{item_qty}</p>
+                        <button id={'add-' + purchase.id} onClick={addHandler} type="button" className="item-qty-btn">+</button>
                     </div>
                 </div>
                 <div className="close-button-container">
-                    <button id={'remove-' + purchase.id} className="item-remove-button"><i className="bi bi-x"></i></button>
+                    <button id={'remove-' + purchase.id} onClick={removeHandler} className="item-remove-button"><i className="bi bi-x"></i></button>
                 </div>
             </div>
         </div>
