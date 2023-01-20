0428a31a3403c20fa9203aeb6640d16cb41f9adb
mscrimaglia <maxiscrimaglia@gmail.com>
2023-01-20 15:53:46 -0300

    item-list-container structure update

--- src/components/item-list-container/index.jsx
+++ src/components/item-list-container/index.jsx
@@ -15,18 +15,6 @@ const ItemListContainer = ({onClose, isOpen}) => {
 
     let items;
 
-    if (localStorage['ch_products_cart'] === undefined || localStorage['ch_products_cart'] === '[]') {
-        items = <h4 className="empty-cart-message">Aún no se ha añadido ningún producto</h4>;
-    }
-    else {
-        let products = JSON.parse(localStorage['ch_products_cart']);
-        items = [];
-        for (let product of products){
-            items.push(<CartItem key={product.id} product={product}/>);
-        }
-        items.push(<h3 key='total_price' id="total_price">Total: ${total_price}</h3>);
-    }
-    
     window.addEventListener('storage', () => {
         let total = 0;
         if (!(localStorage['ch_products_cart'] === undefined || localStorage['ch_products_cart'] === '[]')){
@@ -37,6 +25,29 @@ const ItemListContainer = ({onClose, isOpen}) => {
         setTotalPrice(total);
     });
 
+    if (localStorage['ch_products_cart'] === undefined || localStorage['ch_products_cart'] === '[]') {
+        return (
+            <div className="item_list_container" id="item-list-container" style = {{ transform: isOpen ? 'translateX(0)' : 'translateX(100%)'}}>
+    
+                <div className="close-button-container">
+                    <button onClick={onClose} className="close-button" style = {{ opacity: isOpen ? '100%' : '0%', visibility: isOpen ? 'visible' : 'hidden'}}><i className="bi bi-x"></i></button>
+                </div>
+    
+                <div className="items-container">
+                    <h4 className="empty-cart-message">Aún no se ha añadido ningún producto</h4>
+                </div>
+    
+            </div>
+        )
+    }
+
+    let products = JSON.parse(localStorage['ch_products_cart']);
+    items = [];
+    for (let product of products){
+        items.push(<CartItem key={product.id} product={product}/>);
+    }
+    items.push(<h3 key='total_price' id="total_price">Total: ${total_price}</h3>);
+    
     return (
         <div className="item_list_container" id="item-list-container" style = {{ transform: isOpen ? 'translateX(0)' : 'translateX(100%)'}}>
 
